c06ce2e38f077db857481d45d2052f9125a3e616
Óscar Belmonte <oscar.belmonte@uji.es>
2020-02-22 16:51:09 +0100

    Definida la clase EscalaLineal

---
+++ src/main/java/EscalaLineal.java
@@ -0,0 +1,17 @@
+public class EscalaLineal {
+    final double temperaturaCongelacionAgua;
+    final double temperturaEbullicionAgua;
+
+    public EscalaLineal(final double temperaturaCongelacionAgua, final double temperturaEbullicionAgua) {
+        this.temperaturaCongelacionAgua = temperturaEbullicionAgua;
+        this.temperturaEbullicionAgua = temperturaEbullicionAgua;
+    }
+
+    double temperaturaParaLambda(final double lambda) {
+        return (1 - lambda) * temperaturaCongelacionAgua + lambda * temperturaEbullicionAgua;
+    }
+
+    double lambdaParaTemperatura(final double temperatura) {
+        return (temperatura - temperaturaCongelacionAgua) / (temperturaEbullicionAgua - temperaturaCongelacionAgua);
+    }
+}
