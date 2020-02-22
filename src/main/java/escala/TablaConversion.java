302e0a19db8cc8f176e0b20b9f7ec018d591a58a
Óscar Belmonte <oscar.belmonte@uji.es>
2020-02-22 17:21:53 +0100

    Vamos con el programa principal.

--- src/main/java/escala/TablaConversion.java
+++ src/main/java/escala/TablaConversion.java
@@ -1,4 +1,26 @@
 package escala;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TablaConversion {
+    List<Conversor> conversores = new ArrayList<>();
+
+    public void addConversor(final Conversor conversor) {
+        conversores.add(conversor);
+    }
+
+    public String tabla(final double temperaturaInicio, final double temperaturaFinal, final double salto)  {
+        StringBuffer sb = new StringBuffer();
+        double temperatura = temperaturaInicio;
+
+        while (temperatura <= temperaturaFinal) {
+            for (Conversor conversor: conversores) {
+                sb.append(temperatura + "\t");
+                sb.append(conversor.convierte(temperatura) + "\b");
+            }
+        }
+
+        return sb.toString();
+    }
 }
